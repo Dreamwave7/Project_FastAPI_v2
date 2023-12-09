08b208c1d9edfcd10f6f98d3413a8eb414686b6c
Dreamwave7 <dima190179@gmail.com>
2023-12-10 01:43:40 +0300

    auth

--- app/auth.py
+++ app/auth.py
@@ -1,9 +1,13 @@
+from typing import Annotated
+from fastapi import Depends, HTTPException, status
 from passlib.context import CryptContext
 import jwt
-from datetime import datetime
+from datetime import datetime, timedelta
 from db.database import *
 from sqlalchemy.orm import Session
+from fastapi.security import OAuth2PasswordBearer
 
+oauth = OAuth2PasswordBearer()
 
 
 SECRET_KEY = "9d25e094faa6c9d25e094faa6ca2556c818166b7a9563a2556c818166b7a9563"
@@ -37,5 +41,28 @@ async def authenticate_user(db:Session, username:str, password:str):
     
     return user
 
+async def create_access_token(data:dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=30)
+    to_encode.update({"exp" : expire})
+    encoded_token = jwt.encode(to_encode, SECRET_KEY, ALGM)
+    return encoded_token
 
+async def get_current_user(token:Annotated[str,Depends(oauth)], db:Session = Depends(get_db)):
+    excption = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Cant validate user")
+    data = jwt.decode(token, SECRET_KEY,algorithms=[ALGM])
+    username = data.get("sub")
 
+    if username is None:
+        raise excption
+    
+    user = get_user(db,username)
+    if user is None:
+        raise excption
+    
+    return user
+
+    
+
+    
+    
--- app/routes_users.py
+++ app/routes_users.py
@@ -1,12 +1,6 @@
 from typing import Annotated
 
 from fastapi import Depends, APIRouter
-from fastapi.security import OAuth2PasswordBearer
+
 
 router_users = APIRouter(prefix="/auth", tags=["users"])
-
-auth2 = OAuth2PasswordBearer(tokenUrl="login")
-
-@router_users.get("/test")
-async def read_test(token:Annotated[str, Depends(auth2)]):
-    return {"token":token}
